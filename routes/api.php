1e6d29f3be2e3b2cc62defc363354cd4e68ed926
aisharachmalia <sharachmall@gmail.com>
2024-08-04 16:40:53 +0700

    baru

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,15 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Auth\PemilihLoginController;
+use App\Http\Controllers\KandidatController;
+use App\Http\Controllers\PemilihController;
+use App\Http\Controllers\SuaraController;
+use App\Http\Controllers\UservotingController;
+use App\Http\Controllers\DashboardController;
+use App\Models\Kandidat;
+
+use Illuminate\Support\Facades\Auth;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,4 +25,14 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::middleware(['auth:sanctum'])->group(function () {
+    // Route::post('logout', [AuthController::class, 'logout']);
+    // controller lainnya yang kemarin sudah dibuat simpan dibawah
+    Route::resource('pemilih', PemilihController::class);
+    Route::resource('suara', SuaraController::class);
+    Route::resource('kandidat', KandidatController::class);
+    Route::resource('uservoting', UservotingController::class);
+    // teruskan
 });
